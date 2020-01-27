192cc7c0586e1d4246b9afc9494ef66ea982b767
Ottó Radics <otto@webmenedzser.hu>
2020-01-27 10:29:37 +0100

    config/reporter.php template added

---
+++ src/config.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Reporter plugin for Craft CMS 3.x
+ *
+ * Reporter plugin for Craft CMS.
+ *
+ * @link      https://www.webmenedzser.hu
+ * @copyright Copyright (c) 2020 Ottó Radics
+ */
+
+/**
+ * Reporter config.php
+ *
+ * This file exists only as a template for the Reporter plugin settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'reporter.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [
+    'apiKey' => ''
+];
